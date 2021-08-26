1a05aa77d6843a82cc41b542c31d588984043485
Axm <>
2021-08-26 12:22:38 +0545

    wip

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header>head</header>
+      <main>main</main>
+      <footer>foot</footer>
+    </div>
+  );
+}
+
+export default App;
